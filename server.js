6db1475552497c1cf68ea4118b84fad09b0d4331
Scott Humphries <git@sscotth.io>
2016-02-18 11:37:14 -0600

    extract note routes to own file

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 
+const note = require('./routes/note');
+
 const app = express();
 const port = process.env.PORT || 3000;
-
-const Note = require('./models/note');
 
 app.set('view engine', 'jade');
 
@@ -19,25 +19,7 @@ app.get('/', (req, res) => {
   res.send('Server Running');
 });
 
-app.get('/notes/new', (req, res) => {
-  res.render('new-note');
-});
-
-app.get('/notes/:id', (req, res) => {
-  Note.findById(req.params.id, (err, note) => {
-    if (err) throw err;
-
-    res.render('show-note', {note: note});
-  });
-});
-
-app.post('/notes', (req, res) => {
-  Note.create(req.body, (err, note) => {
-    if (err) throw err;
-
-    res.redirect(`/notes/${note._id}`);
-  });
-});
+app.use(note);
 
 mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
   if (err) throw err;
