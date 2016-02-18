1734a75793143292a8a1e620459e42b9dafa15a2
Scott Humphries <git@sscotth.io>
2016-02-18 13:38:11 -0600

    delete note function

--- routes/note.js
+++ routes/note.js
@@ -5,6 +5,7 @@ const note = require('../controllers/note');
 
 router.get('/notes/new', note.newNote);
 router.get('/notes/:id', note.show);
+router.delete('/notes/:id', note.destroy);
 router.post('/notes', note.create);
 
 module.exports = router;
